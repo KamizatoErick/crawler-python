dd16b624e47bb3cb82effcd293e801a249714bed
Erick Kamizato Scilo <erick.scilo@omie.com.br>
2024-11-05 22:16:45 -0300

    Update ex01.py

--- web-crawler/ex01.py
+++ web-crawler/ex01.py
@@ -1,1 +1,13 @@
-print ('Olá, Mundo')
+import requests
+import re
+
+url = 'https://www.vooo.pro/insights/tutorial-sobre-expressoes-regulares-para-iniciantes-em-python/'
+check = []
+r = requests.get(url)
+html = r.text.encode("utf8")
+search = re.findall(r'<a href=[\'"?](https[://\w\-._]+)', html.decode("utf8"))
+
+for link in search:
+    if link not in check:
+        check.append(link)
+        print(link)
